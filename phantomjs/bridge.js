dd597263389978cd32a44b7259e6d70966152a46
Felix Molitor <felix@whitematter.de>
2013-09-10 11:42:40 +0200

    prefix grunt task message channels with unique id

--- phantomjs/bridge.js
+++ phantomjs/bridge.js
@@ -11,8 +11,11 @@ var url = phantom.args[1];
 var options = JSON.parse(phantom.args[2] || {});
 
 // Messages are sent to the parent by appending them to the tempfile.
+// NOTE, the tempfile appears to be shared between asynchronously running grunt tasks
 var sendMessage = function (arg) {
     var args = Array.isArray(arg) ? arg : [].slice.call(arguments);
+    var channel = options.taskChannelPrefix + '.' + args[0];
+    args[0] = channel;
     fs.write(tmpfile, JSON.stringify(args) + "\n", "a");
 };
 
